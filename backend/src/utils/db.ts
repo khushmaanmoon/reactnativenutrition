a32b7ec7dd2d3234a7476a5c985c0b87ce19706f
Khushmaan <khushmaanchandelpt@gmail.com>
2026-02-10 21:51:30 +0530

    Set the environment variables properly on the deployment server

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,17 +1,16 @@
 import mysql from 'mysql2/promise';
 import fs from 'fs';
 
+const ssl =
+  process.env.DB_CA
+    ? { ca: fs.readFileSync(process.env.DB_CA) }
+    : undefined;
+
 export const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-
-  ssl: {
-    ca: fs.readFileSync(process.env.DB_CA as string),
-  },
-
-  waitForConnections: true,
-  connectionLimit: 10,
+  port: Number(process.env.DB_PORT),
+  ssl,
 });
